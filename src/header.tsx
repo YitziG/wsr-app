39658550bae56899c6a88493cedda05b15a041c7
YitziG <yitzig@wix.com>
2022-05-19 23:07:40 +0300

    Update header.tsx

--- src/header.tsx
+++ src/header.tsx
@@ -1,29 +1,20 @@
 import type React from 'react';
-import { st, classes } from './header.st.css';
-import stylableLogo from './stylable.svg';
+import {
+  classes,
+  st
+} from './header.st.css';
 import { Button } from 'wix-style-react';
 
 export interface HeaderProps {
-    className?: string;
+  className?: string;
 }
 
 export const Header: React.FC<HeaderProps> = ({ className }) => {
-    return (
-        <header className={st(classes.root, className)}>
-            <h1 className={classes.title}>
-                Hello{' '}
-                <a
-                    className={classes.anchor}
-                    href="https://stylable.io"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Stylable!
-                </a>{' '}
-                <img className={classes.icon} src={stylableLogo} width={50} height={50} alt="" />
-            </h1>
-
-            <Button>Click me!</Button>;
-        </header>
-    );
+  return (
+    <header className={st(classes.root, className)}>
+      <h1 className={classes.title}>
+        <Button>Click me!</Button>;
+      </h1>
+    </header>
+  );
 };
